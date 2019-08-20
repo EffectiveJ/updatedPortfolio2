3b118473fdbb1478867df89cf3289d6296f1605a
EffectiveJ <jassaerivera@gmail.com>
2019-08-20 17:35:46 -0400

    Info component added, about page populated

--- portfolio2/src/App.js
+++ portfolio2/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import { Link } from "react-router-dom";
+import Info from "../src/components/info/index.js";
 
 
 function App() {
--- portfolio2/src/pages/about/about.js
+++ portfolio2/src/pages/about/about.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
-
+import Info from "../../components/info/index.js";
+import Nav from "../../components/navbar/index.js";
+import Footer from "../../components/Footer/index.js";
 
 
 function About(){
     return(
         <div>
-
+            <Nav/>
+            <Info/>
+            <Footer/>
         </div>
     )
 }
